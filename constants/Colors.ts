07ed89c5912be02f7d21c1862e92d841d79159eb
Nicolas Alvarez <alvareznicolasdev@gmail.com>
2024-11-19 19:55:11 -0300

    finished project react native

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,9 +1,9 @@
 export const Colors = {
     darkGray: '#2D2D2D',
-    ligthGray: '#9B9B9B',
+    lightGray: '#9B9B9B',
     orange: '#FF9427',
-
+  
     textPrimary: 'white',
     textSecondary: '#666666',
     background: '#000000',
-} as const;
+  } as const;
